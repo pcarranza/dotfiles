600d2b1db00490f6961faade771d3c6889582c27
Pablo Carranza <pcarranza@gmail.com>
2014-04-25 00:03:58 +0100

    Added more stuff

--- activate.sh
+++ activate.sh
@@ -7,8 +7,12 @@ ln -s .dotfiles/gitignore .gitignore
 ln -s .dotfiles/gitmodules .gitmodules 
 ln -s .dotfiles/vim .vim        
 ln -s .dotfiles/vimrc .vimrc        
-ln -s .dotfiles/zsh_aliases .zsh_aliases  
 ln -s .dotfiles/irbrc .irbrc        
+ln -s .dotfiles/zshrc .zshrc
+ln -s .dotfiles/zsh_aliases .zsh_aliases  
+
 git submodule init
 git submodule update
 vim +PluginInstall +qall
+
+ln -s .dotfiles/oh-my-zsh .oh-my-zsh
